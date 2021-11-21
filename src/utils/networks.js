016f7c3bb5e2c45dcad9e22c7eac1904d9dab683
Evan Hallmark <ehallmar@usc.edu>
2021-11-21 13:23:41 -0800

    updates

--- src/utils/networks.js
+++ src/utils/networks.js
@@ -4,7 +4,7 @@ export const mainnet = {
     addressExplorer: "https://mainnet.lamden.io/addresses",
     network: {
         "name": "Lamden Mainnet", 
-        "hosts": ["http://34.133.66.121:3000"], //["https://masternode-01.lamden.io"],
+        "hosts": ["https://proxy.gammaphi.io"], //["https://masternode-01.lamden.io"],
         "type": "mainnet", 
         "lamden": true, 
         "currencySymbol": "TAU",
@@ -18,7 +18,7 @@ export const testnet = {
 
     network: {
         "name": "Lamden Testnet", 
-        "hosts": ["http://34.133.66.121:3000"],//["https://testnet-master-1.lamden.io"], 
+        "hosts": ["https://proxy.gammaphi.io"],//["https://testnet-master-1.lamden.io"], 
         "type": "testnet", 
         "lamden": true, 
         "currencySymbol": "dTAU",
