dd9b827644e788964ee387a621413f49e5ff8129
Evan Hallmark <ehallmar@usc.edu>
2021-12-03 11:13:51 -0800

    update width on svg

--- src/components/Tokens/Token_Custom.svelte
+++ src/components/Tokens/Token_Custom.svelte
@@ -2,8 +2,14 @@
     export let base64Image, type="svg+xml", alt="";
 </script>
 
-
+<style>
+img.image-icon {
+    width: 100%;
+    height: 100%;
+}
+</style>
 <img
+    class="image-icon"
     src="data:image/{type};base64, {base64Image}"
     alt={alt}
 />
